28cc0c5a87ec0ec3d8087364ec3486bf421e41d7
S B <barat.shivansi@gmail.com>
2020-08-03 20:59:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+Heroes = {
+    "AAA" : "Starlord",
+    "AAB" : "Drax",
+    "ABA" : "Black Panther",
+    "ABB" : "Doctor Strange",
+    "BAA" : "Winter Soldier",
+    "BAB" : "Rocket Raccoon",
+    "BBA" : "Captain America",
+    "BBB" : "Iron Man",
+    "CAA" : "Groot",
+    "CAB" : "Hulk",
+    "CBA" : "Spiderman",
+    "CBB" : "Mantis",
+    "DAA" : "Thanos",
+    "DAB" : "Thor",
+    "DBA" : "Hawkeye",
+    "DBB" : "Black Widow"
+}
